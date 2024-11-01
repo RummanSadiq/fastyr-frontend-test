173f6e9cef99afb9be4476a7ebafc02058c52a1e
RummanSadiq <hey.rummansadiq@gmail.com>
2024-11-01 18:21:09 +0500

    refactor: add common confirmation modal

--- src/views/userdetails/index.tsx
+++ src/views/userdetails/index.tsx
@@ -1,68 +1,92 @@
-'use client';
+"use client";
 
 import { FunctionComponent, useCallback, useState } from "react";
 import classNames from "classnames";
+import { useMutation } from "@apollo/client";
 
 // Custom Components
 import { UserInfoCard } from "@/components/users/UserInfoCard";
 import { Button } from "@/components/ui/button";
-import { DeleteUserModal } from "@/components/users/DeleteUserModal";
 import { CreateUserModal } from "@/components/users/CreateUserModal";
+import { ConfirmationModal } from "@/components/common/ConfirmationModal";
 
 // Types
 import { User } from "@/types/users";
+
+// Services
+import { DeleteUser } from "@/services/users";
 
 type Props = {
   user: User;
 };
 
 export const UserDetailsView: FunctionComponent<Props> = ({ user }) => {
-  const [openUpdateUserModal, setUpdateUserModal] = useState<boolean>(false)
+  const [openUpdateUserModal, setUpdateUserModal] = useState<boolean>(false);
   const [isDeleteModalOpen, setDeleteModalOpen] = useState(false);
 
+  const [deleteUser, { loading }] = useMutation(DeleteUser);
+
   const toggleUpdateUserModal = useCallback(() => {
-    setUpdateUserModal((prev) => !prev)
-  },[])
+    setUpdateUserModal((prev) => !prev);
+  }, []);
 
   const handleOpenDeleteModal = useCallback(() => {
-    setDeleteModalOpen(true)
-  },[])
+    setDeleteModalOpen(true);
+  }, []);
 
   const handleCloseDeleteModal = useCallback(() => {
-    setDeleteModalOpen(false)
-  },[])
+    setDeleteModalOpen(false);
+  }, []);
+
+  const handleDeleteConfirm = useCallback(async () => {
+    await deleteUser({ variables: { id: user.id } });
+    handleCloseDeleteModal();
+  }, [deleteUser, handleCloseDeleteModal, user.id]);
 
   return (
     <div>
-      <UserInfoCard 
-        name={user.name} 
-        email={user.email} 
-        website={user.website} 
-        phone={user.phone} 
-        username={user.username} 
+      <UserInfoCard
+        name={user.name}
+        email={user.email}
+        website={user.website}
+        phone={user.phone}
+        username={user.username}
       />
       <div className={classNames("mt-4 flex justify-between")}>
-        <Button 
-          onClick={toggleUpdateUserModal} 
-          className={classNames("bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600")}
+        <Button
+          onClick={toggleUpdateUserModal}
+          className={classNames(
+            "rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600",
+          )}
         >
           Update
         </Button>
-        <Button 
-          onClick={handleOpenDeleteModal} 
-          className={classNames("bg-red-500 text-white py-2 px-4 rounded hover:bg-red-600 ml-2")}
+        <Button
+          onClick={handleOpenDeleteModal}
+          className={classNames(
+            "ml-2 rounded bg-red-500 px-4 py-2 text-white hover:bg-red-600",
+          )}
         >
           Delete
         </Button>
       </div>
 
-      {openUpdateUserModal && <CreateUserModal onClose={toggleUpdateUserModal} user={user}/>}
+      {openUpdateUserModal && (
+        <CreateUserModal onClose={toggleUpdateUserModal} user={user} />
+      )}
 
-      {isDeleteModalOpen && <DeleteUserModal 
-        isOpen={isDeleteModalOpen} 
-        onClose={handleCloseDeleteModal} 
-        userId={user.id} 
-      />}
+      {isDeleteModalOpen && (
+        <ConfirmationModal
+          title={"Confirm Deletion"}
+          description={
+            "Are you sure you want to delete this user? This action cannot be undone."
+          }
+          isLoading={loading}
+          isOpen={isDeleteModalOpen}
+          handleClose={handleCloseDeleteModal}
+          handleConfirm={handleDeleteConfirm}
+        />
+      )}
     </div>
   );
 };
